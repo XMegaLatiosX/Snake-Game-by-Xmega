3561e11cce3aef0e59f3b37bb0b5c9ac3ab1d5aa
Júlyin 8) <86024663+XMegaLatiosX@users.noreply.github.com>
2022-08-07 16:11:05 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ const canvas = ids("canvas")
 canvas.width    = 1024
 canvas.height   = 576
 const c = canvas.getContext("2d")
+let interval = 600
 let playing = false
+
+
 const background = new Sprite ({
     position: {x:0, y:0},
     imgSrc: "./assets/bg.png"
@@ -11,27 +14,120 @@ let player = new Head ({
     position: {x:4 * 64, y: 4 * 64},
     imgSrc: "./assets/head-right.png"
 })
-
 const fruit = new Apple ({
     position: {x: Math.floor(Math.random() * (canvas.width / 64)) * 64 + 12,
                y: Math.floor(Math.random() * (canvas.height / 64))* 64 + 12},
     imgSrc: "./assets/apple.png",
     scale: 0.5
 })
-function draw() {
+
+
+function load() {
     background.draw()
 }
+
+function settingsTab() {
+    //MAIN DIV FOR CONFIG SCREEN
+    const configDiv = document.createElement('div')
+    configDiv.className = "configDiv"
+
+    //TELA DE CONFIGURAÇÃO
+    const configScreen = document.createElement('div')
+    configScreen.className = "configScreen"
+
+    //div com os botões
+    const optionsDiv = document.createElement('div')
+    optionsDiv.className = "optionsDiv"
+
+    //
+    const setDifficulty = document.createElement('div')
+    setDifficulty.className = "setDiffDiv"
+
+
+
+    const spdUp = document.createElement('button')
+    spdUp.className = "spdBtn down"
+    spdUp.innerHTML = "◂"
+    spdUp.onclick = () => {changeSpd(+100)}
+
+    const spdDisplay = document.createElement('span') //EXIBI A VELOCIDADE
+    spdDisplay.className = "spdDisplay"
+    spdDisplay.innerHTML = "velocidade: " + (11 - interval / 100)
+
+    const spdDown = document.createElement('button')
+    spdDown.className = "spdBtn up"
+    spdDown.innerHTML = "▸"
+    spdDown.onclick = () => {changeSpd(-100)}
+
+    if(interval == 100) spdDown.disabled = true
+    if(interval == 1000) spdUp.disabled = true
+
+    const backBtn = document.createElement('button')
+    backBtn.className = "backBtn"
+    backBtn.innerHTML = '⮭'
+    backBtn.onclick = () => {close_config()}
+    
+
+
+    ids(".content").appendChild(configDiv)
+
+    configDiv.appendChild(configScreen)
+
+    configScreen.appendChild(backBtn)
+    configScreen.appendChild(optionsDiv)
+
+    optionsDiv.appendChild(setDifficulty)
+
+    setDifficulty.appendChild(spdDisplay)
+    setDifficulty.appendChild(spdDown)
+    setDifficulty.appendChild(spdUp)
+
+    function close_config() {
+        ids(".content").removeChild(configDiv)
+    }
+    function changeSpd(value) {
+        interval += value
+        spdDisplay.innerHTML = "velocidade: " + (11 - interval / 100)
+        if(interval == 1000) {
+            spdUp.disabled = true
+        }else {
+            spdUp.disabled = false
+        }
+        if(interval == 100) {
+            spdDown.disabled = true
+        }else {
+            spdDown.disabled = false
+        }
+    }
+}
+
+
 function start() {
-    ids(".title").style.display = "none"
-    ids(".btnPlay").style.display = "none"
+    title.style.display = "none"
+    btn.style.display = "none"
     update()
 }
-//-------------------------------------------------------update()------------------------------------------//
+function gameOver(points) {
+    if(points >= 140) {
+        title.innerHTML = "YOU WON!!"
+        btn.innerHTML = "GG"
+    } else {
+        title.innerHTML = "DEAD."
+        btn.innerHTML = "retry!"
+    }
+    title.innerHTML += " points: " + (points - 2)
+    btn.onclick = function() {window.location.reload()}
+    btn.style.display = "block"
+    title.style.display = "block"
+}
 
+//-------------------------------------------------------update()------------------------------------------//
 let timerId
 function update() {
-    timerId = setTimeout(update, 150)
-    background.update()
-    player.update()
-    fruit.update()
+    if(!player.dead){
+        timerId = setTimeout(update, interval)
+        background.draw()
+        player.update()
+        fruit.draw()
+    }
 }
