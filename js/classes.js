4963e0e244139452f0a3108459dce6632f995f96
Júlyin 8) <86024663+XMegaLatiosX@users.noreply.github.com>
2022-08-07 16:16:50 -0300

    Add files via upload

--- js/classes.js
+++ js/classes.js
@@ -1,20 +1,22 @@
 class Sprite {
     constructor({position, imgSrc, scale = 1}) {
         this.pos = position
-        this.img = new Image()//declara que .img é uma imagem
+        this.img = new Image()//define .img como um objeto Image
         this.img.height
         this.img.width
-        this.img.src = imgSrc //assim que a fonte é passada a altura e o comprimento são definidos automaticamente
+        //assim que a fonte é passada a altura e o comprimento são definidos automaticamente no this.img
+        this.img.src = imgSrc
         this.scale = scale
-    }
-    //tudo relacionado a desenhar vai ser chamado aqui.
+    } 
     draw() {
         //desenha a imagem
         //os parametros de draw image são: ("O" é de opcional, e entre paranteses são os de segunda prioridade)
-        //imagem,(corte X, corte y, corte width, corte height) x, y,O width,O height.
+        //imagem,O:(corte X, corte y, corte width, corte height) x, y,O: width,O: height.
         c.drawImage(this.img, this.pos.x, this.pos.y, this.img.width * this.scale, this.img.height * this.scale)
     }
     update() {
+        //essa parte é importante pro desenho inteiro ser atualizado constantemente,
+        //caso contrario alguns sprites irao se repetir
         this.draw()
     }
 }
@@ -28,79 +30,104 @@ class Head extends Sprite {
             new Body({position: {x: this.pos.x -64, y: this.pos.y}, imgSrc: "./assets/snk-b1.png", scale: 1, arrayNum: 0}),
             new Body({position: {x: this.pos.x -128, y: this.pos.y}, imgSrc: "./assets/snk-b2.png", scale: 1, arrayNum: 1})
         ]
-        this.bImg = "./assets/snk-b1.png"
+        // this.deadImg1 = new Image()
+        this.bodyImg = "./assets/snk-b1.png"
         this.pos = position
         this.width = 16 * this.scale
         this.height = 16 * this.scale
         this.velocity = {x:64, y:0}
         this.dead = false
 
-        this.direction = "./assets/head-right.png"
-        this.joj = {}
-        this.jaj = {}
-        this.consoleCounter = 0
-        this.counter = 1
+        this.direction = "right"
+        //salvam as posições anterioes a da ultima cauda 
+        this.prevPos = {}
+        this.nextPos = {}
     }
     add() {
         //adiciona 1 comprimento de cauda com a posição anterior da ultima cauda
-        this.counter++
-        this.tail.push(new Body({position: {x: this.jaj.x,y: this.jaj.y}, imgSrc: this.bImg, scale: 1, arrayNum: this.counter}))
-        if(this.bImg == "./assets/snk-b1.png") {
-            this.bImg = "./assets/snk-b2.png"
+        this.tail.push(new Body({
+            position: {x: this.nextPos.x,y: this.nextPos.y},
+            imgSrc: this.bodyImg,
+            scale: 1,
+            arrayNum: this.tail.length
+        }))
+        //alterna o sprite das caudas
+        if(this.bodyImg == "./assets/snk-b1.png") {
+            this.bodyImg = "./assets/snk-b2.png"
         }else {
-            this.bImg = "./assets/snk-b1.png"
+            this.bodyImg = "./assets/snk-b1.png"
         }
     }
 
     die() {
+        //declara q o player morreu.
         this.dead = true
-        if(this.tail.length >= 140) {
-            ids(".title").innerHTML = "YOU WON!!"
-            ids(".btnPlay").innerHTML = "GG"
-        } else {
-            ids(".title").innerHTML = "DEAD."
-            ids(".btnPlay").innerHTML = "retry!"
+        
+        this.pos.x = this.tail[0].pos.x
+        this.pos.y = this.tail[0].pos.y
+        this.c = 0
+        while(this.c <= this.tail.length - 1){
+            if(this.c != this.tail.length - 1) {
+                this.tail[this.c].pos.x = this.tail[this.c + 1].pos.x
+                this.tail[this.c].pos.y = this.tail[this.c + 1].pos.y
+            } else {
+                this.tail[this.c].pos.x = this.nextPos.x
+                this.tail[this.c].pos.y = this.nextPos.y
+            }
+            this.tail[this.c].draw()
+            this.c++
         }
-        ids(".btnPlay").onclick = function() {window.location.reload()}
-        ids(".btnPlay").style.display = "block"
-        ids(".title").style.display = "block"
+        if(this.pos.x + spd == this.tail[0].pos.x) {
+            this.img.src = "./assets/dead-left.png"
+        }else
+        if(this.pos.x - spd == this.tail[0].pos.x) {
+            this.img.src = "./assets/dead-right.png"
+        }else
+        if(this.pos.y + spd == this.tail[0].pos.y) {
+            this.img.src = "./assets/dead-up.png"
+        }else
+        if(this.pos.y - spd == this.tail[0].pos.y) {
+            this.img.src = "./assets/dead-down.png"
+        }
+        this.draw()
+
+        gameOver(this.tail.length)
     }
     update() {
-        //colisão com paredes do mapa
-        if( this.pos.x + this.width > canvas.width ||
-            this.pos.y + this.height > canvas.height||
-            this.pos.x < 0 ||
-            this.pos.y < 0) {this.die();
-        }
-        //colisão com a fruta
-        if( this.pos.x + this.width > fruit.pos.x  &&
-            this.pos.y + this.height > fruit.pos.y &&
-            this.pos.x < fruit.pos.x + fruit.width &&
-            this.pos.y < fruit.pos.y + fruit.height) {fruit.eat(this)
-        }
-        //atualiza todas as caudas
-        this.i= this.counter
-        while(this.i > -1){
-            //update 4 3 2 1...
-            this.tail[this.i].update(this)
-            this.i--
-        }
-
-        this.jaj.x = this.joj.x
-        this.jaj.y = this.joj.y
-        this.joj.x = this.tail[this.counter].pos.x
-        this.joj.y = this.tail[this.counter].pos.y
-        // c.drawImage()
-        this.draw()
         if(!this.dead) {
-            this.pos.x += this.velocity.x
-            this.pos.y += this.velocity.y
-            this.img.src = this.direction
-            this.i= this.counter
+            this.i= this.tail.length - 1
+             while(this.i > -1){
+                 if(this.tail[this.i].checkCollider(this)) {this.die(); return}
+                 this.i--
+             }
+            //colisão com paredes do mapa
+             if( this.pos.x + this.width > canvas.width ||
+                 this.pos.y + this.height > canvas.height||
+                 this.pos.x < 0 ||
+                 this.pos.y < 0) {this.die(); return}
+            //colisão com a fruta
+            if( this.pos.x + this.width > fruit.pos.x  &&
+                this.pos.y + this.height > fruit.pos.y &&
+                this.pos.x < fruit.pos.x + fruit.width &&
+                this.pos.y < fruit.pos.y + fruit.height) {fruit.eat(this)
+            }
+            //atualiza todas as caudas em ordem decrescente, para que cada uma pegue a posição PASSADA da próxima cauda
+            this.i = this.tail.length - 1
             while(this.i > -1){
-                this.tail[this.i].died(this)
+                this.tail[this.i].update(this)
                 this.i--
             }
+    
+            // c.drawImage()
+            this.draw()
+            this.nextPos.x = this.prevPos.x
+            this.nextPos.y = this.prevPos.y
+            this.prevPos.x = this.tail[this.tail.length - 1].pos.x
+            this.prevPos.y = this.tail[this.tail.length - 1].pos.y
+
+            this.pos.x += this.velocity.x
+            this.pos.y += this.velocity.y
+            this.img.src = "./assets/head-"+this.direction+".png"
         }
     }
 }
@@ -115,20 +142,24 @@ class Body extends Sprite{
     }
     update(plClass) {
         this.draw()
+        //pega posição da prolxima cauda, muda de lugar
         if(this.id == 0) {
             this.pos.x = plClass.pos.x
             this.pos.y = plClass.pos.y
-        }
-        if(this.id >0){
+        }else{
             this.pos.x = plClass.tail[this.id -1].pos.x
             this.pos.y = plClass.tail[this.id -1].pos.y
         }
     }
-    died(plClass) {
-        if( this.pos.x + this.width > plClass.pos.x &&
-            this.pos.y + this.height > plClass.pos.y &&
-            this.pos.x < plClass.pos.x + plClass.width &&
-            this.pos.y < plClass.pos.y + plClass.height){plClass.die()}
+    checkCollider(obj) {
+        //retorna verdadeiro se o objeto passado está collidindo com o corpo
+        //pro código proceder com base nessa informação
+        if( this.pos.x + this.width > obj.pos.x &&
+            this.pos.y + this.height > obj.pos.y &&
+            this.pos.x < obj.pos.x + obj.width &&
+            this.pos.y < obj.pos.y + obj.height){return true}
+        //se a condição acima for falsa, retornara falso
+        return false
     }
 }
 
@@ -138,7 +169,6 @@ class Apple extends Sprite{
         this.pos = position
         this.width = 16  * this.scale
         this.height = 16 * this.scale
-        this.jaj = 0
     }
 
     eat(playerClass) {
@@ -146,21 +176,17 @@ class Apple extends Sprite{
         playerClass.add()
     }
     changePos(plClass) {
-        console.log("TROQUEI" + this.jaj++)
         this.pos = {x: Math.floor(Math.random() * (canvas.width / 64)) * 64 + 12,
                     y: Math.floor(Math.random() * (canvas.height / 64))* 64 + 12}
         this.i = 0
-        while(this.i < plClass.counter) {
-            if( this.pos.x + this.width > plClass.tail[this.i].pos.x &&
-                this.pos.y + this.height > plClass.tail[this.i].pos.y &&
-                this.pos.x < plClass.tail[this.i].pos.x + plClass.tail[this.i].width &&
-                this.pos.y < plClass.tail[this.i].pos.y + plClass.tail[this.i].height){
-                this.pos = {x: Math.floor(Math.random() * (canvas.width / 64)) * 64 + 12,
-                            y: Math.floor(Math.random() * (canvas.height / 64))* 64 + 12}}
+        while(this.i < plClass.tail.length - 1) {
+            if(plClass.tail[this.i].checkCollider(this)){this.changePos(plClass)}
             this.i++
         }
-    }
-    update() {
-        this.draw()
+        //se a fruta nascer dentro da caebça ira trocar de posição
+        if( this.pos.x + this.width > plClass.pos.x &&
+            this.pos.y + this.height > plClass.pos.y &&
+            this.pos.x < plClass.pos.x + plClass.width &&
+            this.pos.y < plClass.pos.y + plClass.height){this.changePos(plClass)}
     }
 }
--- js/utilities.js
+++ js/utilities.js
@@ -1,36 +1,52 @@
 function ids(id) {
     return document.querySelector(id)
 }
+const title = ids(".title")
+const btn = ids(".btnPlay")
+
+
+const preload = [new Image(), new Image(), new Image(), new Image(), new Image()]
+preload[0].src = "./assets/dead-up.png"
+preload[1].src = "./assets/dead-down.png"
+preload[2].src = "./assets/dead-left.png"
+preload[3].src = "./assets/dead-right.png"
+preload[4].src = "./assets/settings1.png"
+
+//movimentação/troca de sprite
 const spd = 64
 window.addEventListener('keydown', (event) =>{
     
     switch (event.key) {
         case 'w':
+        case 'W':
         case 'ArrowUp':
             if(player.pos.y - spd != player.tail[0].pos.y) {
                 player.velocity = {x: 0, y: -spd}
-                player.direction = "./assets/head-up.png"
+                player.direction = "up"
             }
             break;
         case 's':
+        case 'S':
         case 'ArrowDown':
             if(player.pos.y + spd != player.tail[0].pos.y) {
                 player.velocity = {x: 0, y: spd}
-                player.direction = "./assets/head-down.png"
+                player.direction = "down"
             }
             break;
         case 'a':
+        case 'A':
         case 'ArrowLeft':
             if(player.pos.x - spd != player.tail[0].pos.x) {
                 player.velocity = {x: -spd, y: 0}
-                player.direction = "./assets/head-left.png"
+                player.direction = "left"
             }
             break;
         case 'd':
+        case 'D':
         case 'ArrowRight':
             if(player.pos.x + spd != player.tail[0].pos.x) {
                 player.velocity = {x: spd, y: 0}
-                player.direction = "./assets/head-right.png"
+                player.direction = "right"
             }
             break;
     }
